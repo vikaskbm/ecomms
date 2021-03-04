036e3e80b2670ebc8129207c30bf5e7cc2bfb3b4
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-04 21:59:02 +0530

    Added label and category and label choices and updated Item model

--- core/models.py
+++ core/models.py
@@ -2,9 +2,23 @@ from django.conf import settings
 from django.db import models
 
 
+CATEGORY_CHOICES =  (
+    ('S', 'Shirt'),
+    ('SW', 'Sports Wear'),
+    ('OW', 'Out Wear'),
+) 
+
+LABEL_CHOICES =  (
+    ('p', 'primary'),
+    ('s', 'secondary'),
+    ('d', 'danger'),
+) 
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def __str__(self):
         return self.title
