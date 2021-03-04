f22099d9286b8428aded0e87b42d5e1bbbfdaf7e
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-04 21:27:54 +0530

    Added model for Item

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
