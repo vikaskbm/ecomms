bc27ece1ebc266a53e7dd4b9f39f90e100842115
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-04 21:28:24 +0530

    Added model for Order

--- core/models.py
+++ core/models.py
@@ -17,3 +17,12 @@ class OrderItem(models.Model):
         return self.title
 
 
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    order_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
