25f40efa118a7c5b1cbc36e8133968c4e62a2378
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-05 13:06:41 +0530

    added views for detailview of item and checkout page

--- core/views.py
+++ core/views.py
@@ -7,3 +7,11 @@ class HomeView(ListView):
     model = Item
     template_name = 'home.html'
 
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
+
+
+def checkout(request):
+    return render(request, "checkout.html")
