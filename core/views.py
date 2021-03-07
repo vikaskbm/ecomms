609104594b0c3dfe300bd4a6cbf24e1636fe2543
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-07 18:24:06 +0530

    added functionality to increment/decrement and remove items from order_summary page

--- core/views.py
+++ core/views.py
@@ -55,18 +55,18 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "Item quantity was updated")
-            return redirect("core:checkout")
+            return redirect('core:order_summary')
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart")
-            return redirect("core:product", slug=slug)
+            return redirect('core:order_summary')
     
     else:
         order_date = timezone.now()
         order = Order.objects.create(user=request.user, order_date=order_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart")
-        return redirect("core:product", slug=slug)
+        return redirect("core:order_summary" )
 
 
 @login_required
@@ -86,7 +86,7 @@ def remove_from_cart(request, slug):
             order_item.quantity = 0
             order.items.remove(order_item)
             messages.info(request, "This item was removed from your cart")
-            return redirect('core:product', slug=slug)
+            return redirect('core:order_summary')
 
         else:
             messages.info(request, "This item was not in your cart")
@@ -112,8 +112,10 @@ def remove_single_item_from_cart(request, slug):
                 user=request.user,
                 ordered=False
             )[0]
-            if order_item.quantity > 0:
+
+            if order_item.quantity > 1:
                 order_item.quantity -= 1
+                order_item.save()
             else:
                 remove_from_cart(request, slug)
                 
@@ -122,9 +124,9 @@ def remove_single_item_from_cart(request, slug):
 
         else:
             messages.info(request, "This item was not in your cart")
-            return redirect('core:product')
+            return redirect('core:product', slug=slug)
 
     else:
         # order does not exist
         messages.info(request, "You do not have an active order")
-        return redirect('core:product')
+        return redirect('core:product', slug=slug)
